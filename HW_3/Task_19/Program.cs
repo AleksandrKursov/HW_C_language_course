8a48efec237ae01cb4136f5bc8c4ccc6806082bb
Aleksandr Kursov <alexander.kursov@gmail.com>
2023-03-11 15:41:02 +0500

    HW_3_C#

---
+++ HW_3/Task_19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Input five-digit number: ");
+string? str = Console.ReadLine();
+if (str!.Length == 5)
+{
+    if(str[0] == str[4] && str[1] ==str[3])
+    {
+        System.Console.WriteLine("Yes, it is a polyndrom number");
+    }
+    else{
+        System.Console.WriteLine("This number is not a polydrome");
+    } 
+}
+else
+{
+    System.Console.WriteLine("Number is not five-digit");
+}
---
+++ HW_3/Task_21/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int[] coordsA = new int[3];
+
+System.Console.WriteLine("Input the X coordinate of point A: ");
+
+coordsA[0] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Input the Y coordinate of point A:  ");
+
+coordsA[1] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Input the Z coordinate of point A:  ");
+
+coordsA[2] = Convert.ToInt32(Console.ReadLine());
+
+int[] coordsB = new int[3];
+
+System.Console.WriteLine("Input the X coordinate of point B: ");
+
+coordsB[0] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Input the Y coordinate of point B:  ");
+
+coordsB[1] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Input the Z coordinate of point B:  ");
+
+coordsB[2] = Convert.ToInt32(Console.ReadLine());
+
+int distanceX = (int)Math.Pow(coordsB[0] - coordsA[0], 2);
+int distanceY = (int)Math.Pow(coordsB[1] - coordsA[1], 2);
+int distanceZ = (int)Math.Pow(coordsB[2] - coordsA[2], 2);
+
+double result = Math.Sqrt(distanceX + distanceY + distanceZ);
+
+System.Console.WriteLine(Math.Round(result, 2));
